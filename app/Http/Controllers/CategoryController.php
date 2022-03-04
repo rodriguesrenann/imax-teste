238110359b1106d6ac2a1904dd5ae1eff990e723
Renan Rodrigues <renanrodrigues_cb_@hotmail.com>
2022-03-03 21:44:31 -0300

    adicionando validator ao criar categoria

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,6 +6,7 @@ use App\Http\Resources\CategoryResource;
 use App\Repositories\CategoryRepository;
 use App\Services\JsonFormatter;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends Controller
 {
@@ -18,12 +19,21 @@ class CategoryController extends Controller
 
     public function index()
     {
-
     }
 
     public function create(Request $request, JsonFormatter $formater)
     {
         $data = $formater->formatJsonToArray($request->json);
+
+        $validator = Validator::make($data, [
+            'name' => 'max:30'
+        ]);
+
+        if ($validator->fails()) {
+            return  response()->json([
+                'error' => $validator->errors()->first()
+            ], 400);
+        }
 
         return new CategoryResource($this->repository->insertCategory($data));
     }
